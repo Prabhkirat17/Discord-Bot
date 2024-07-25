12c5984cddcab45bd40abce8eb319c8fc64eb8c0
Apple <apple@apples-air.home>
2024-07-25 18:02:48 -0400

    Initial commit: Add Discord bot project

---
+++ bot.py
@@ -0,0 +1,31 @@
+# bot.py
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+# Define intents
+intents = discord.Intents.default()
+intents.message_content = True  # Enable the message content intent if you need to read message content
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+    
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+
+client.run(TOKEN)
